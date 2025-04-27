ed7f28b3fa1f2ce752787970b26a754c90cbcab5
Favio10 <favio_olivera@hotmail.com>
2025-04-27 06:53:23 -0300

    edicion de rutas1

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes/articlesRoutes.js');
+const articlesRoutes = require('./routes/articlesRoutes.js');
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/', routes)
+app.use('/articulos', articlesRoutes)
 
 module.exports = app;
